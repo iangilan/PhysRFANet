29d38cf26b3bcb3d27ce759b24e1aa2e9047c204
iangilan <31282602+iangilan@users.noreply.github.com>
2023-12-12 16:57:53 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -56,5 +56,64 @@ def new_combined_loss(pred, target, alpha, beta, gamma):
     dice = dice_loss(pred, target)
     return alpha * mse + beta * wmse + gamma * dice
 
-# You can add more utility functions here as needed.
 
+class conv_block(nn.Module):
+    def __init__(self,ch_in,ch_out):
+        super(conv_block,self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv3d(ch_in, ch_out, kernel_size=3,stride=1,padding=1,bias=True),
+            nn.BatchNorm3d(ch_out),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(ch_out, ch_out, kernel_size=3,stride=1,padding=1,bias=True),
+            nn.BatchNorm3d(ch_out),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self,x):
+        x = self.conv(x)
+        return x
+
+    
+class up_conv(nn.Module):
+    def __init__(self,ch_in,ch_out,size,stride,padding):
+        super(up_conv,self).__init__()
+        self.up = nn.Sequential(
+            nn.Upsample(size=size),
+            nn.Conv3d(ch_in,ch_out,kernel_size=3,stride=stride,padding=padding,bias=True),
+            nn.BatchNorm3d(ch_out),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self,x):
+        x = self.up(x)
+        return x
+
+
+class Attention_block(nn.Module):
+    def __init__(self,F_g,F_l,F_int):
+        super(Attention_block,self).__init__()
+        self.W_g = nn.Sequential(
+            nn.Conv3d(F_g, F_int, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm3d(F_int)
+            )
+        
+        self.W_x = nn.Sequential(
+            nn.Conv3d(F_l, F_int, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm3d(F_int)
+        )
+
+        self.psi = nn.Sequential(
+            nn.Conv3d(F_int, 1, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm3d(1),
+            nn.Sigmoid()
+        )
+        
+        self.relu = nn.ReLU(inplace=True)
+        
+    def forward(self,g,x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1+x1)
+        psi = self.psi(psi)
+
+        return x*psi
