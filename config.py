6a16c08c7ca373ff6950600cad14e74769e106f3
iangilan <31282602+iangilan@users.noreply.github.com>
2023-12-18 16:34:54 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 # Configuration settings
-num_epochs = 1
+num_epochs = 200
 batch_size = 16 # Batch size for DataLoader
 
 # weights for the combined loss function
-alpha = 0.8
+alpha = 0.6
 beta  = 0.1
-gamma = 0.1
+gamma = 0.3
 
 # model path
 model_path_Temp = "models_Temp" # Replace with your actual Temperature model path
 model_path_Dmg  = "models_Dmg"  # Replace with your actual Damage model path
 
-# data/model path
+# training/test data path
 data_path = "data"  # Replace with your actual data path
 file_paths = {
     'Temp_train'  : data_path + '/data_Temp/Temp_train.npy',
