486b74eb25dc37f2cf9b5d3ce5ac754b65f58133
iangilan <31282602+iangilan@users.noreply.github.com>
2023-12-21 14:57:10 +0900

    Update data_loader_Temp.py

--- data_loader_Temp.py
+++ data_loader_Temp.py
@@ -48,17 +48,17 @@ def load_data(file_paths):
     # Split the data into training and test sets
     Temp_train_data, Temp_test_data_foreseen = train_test_split(Temp_train_data, test_size=500, random_state=42)
     Temp_test_data_unforeseen, Temp_test_data_dummy = train_test_split(Temp_test_data, test_size=500, random_state=42)
-    Temp_train_data, Temp_valid_data = train_test_split(Temp_train_data, test_size=1000, random_state=42)
+    Temp_train_data, Temp_valid_data = train_test_split(Temp_train_data, test_size=200, random_state=42)
 
     # Split the data into training and test sets
     Ninput_train_data, Ninput_test_data_foreseen = train_test_split(Ninput_train_data, test_size=500, random_state=42)
     Ninput_test_data_unforeseen, Ninput_test_data_dummy = train_test_split(Ninput_test_data, test_size=500, random_state=42)
-    Ninput_train_data, Ninput_valid_data = train_test_split(Ninput_train_data, test_size=1000, random_state=42)
+    Ninput_train_data, Ninput_valid_data = train_test_split(Ninput_train_data, test_size=200, random_state=42)
 
     # Split the data into training and test sets
     MR_train_data, MR_test_data_foreseen = train_test_split(MR_train_data, test_size=500, random_state=42)
     MR_test_data_unforeseen, MR_test_data_dummy = train_test_split(MR_test_data, test_size=500, random_state=42)
-    MR_train_data, MR_valid_data = train_test_split(MR_train_data, test_size=1000, random_state=42)
+    MR_train_data, MR_valid_data = train_test_split(MR_train_data, test_size=200, random_state=42)
     
     return Temp_train_data, Ninput_train_data, MR_train_data, 
     Temp_valid_data, Ninput_valid_data, MR_valid_data, 
